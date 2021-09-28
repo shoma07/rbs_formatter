9a0a4cd0346199a83fada128da8fd378f32a8534
shoma07 <23730734+shoma07@users.noreply.github.com>
2021-09-29 01:31:57 +0900

    initial commit

---
+++ lib/rbs_formatter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rbs_formatter/version"
+
+module RbsFormatter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rbs_formatter_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RbsFormatter do
+  it "has a version number" do
+    expect(RbsFormatter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
